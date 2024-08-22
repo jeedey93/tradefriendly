32abdd6b264d00b9186b75afdff0ab05e2de4f5c
Justin Do <justin.do@hotmail.com>
2024-08-22 19:20:36 -0400

    Test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Teams from "./pages/Teams";
