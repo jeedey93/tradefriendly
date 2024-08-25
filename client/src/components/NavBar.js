e044d2c53223014d8fba14e357bb820f396200ee
Justin Do <justin.do@hotmail.com>
2024-08-25 11:35:19 -0400

    Fix fix

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,25 +1,40 @@
 import React, { useState } from "react";
-import React, { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Box from "@mui/material/Box";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Container,
+  Menu,
+  MenuItem,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  CssBaseline,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import SportsHockeyIcon from "@mui/icons-material/SportsHockey";
-import { CssBaseline, Container, Menu, MenuItem } from "@mui/material";
-import { CssBaseline, Container, Menu, MenuItem } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from "@mui/icons-material/Home";
+import PeopleIcon from "@mui/icons-material/People";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
 
 function NavBar() {
-  // State for Tool menu
   const [toolAnchorEl, setToolAnchorEl] = useState(null);
   const isToolMenuOpen = Boolean(toolAnchorEl);
 
-  // State for Admin menu
   const [adminAnchorEl, setAdminAnchorEl] = useState(null);
   const isAdminMenuOpen = Boolean(adminAnchorEl);
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const handleToolMenuClick = (event) => {
     setToolAnchorEl(event.currentTarget);
@@ -37,12 +52,31 @@ function NavBar() {
     setAdminAnchorEl(null);
   };
 
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
     <>
       <CssBaseline />
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        sx={{
+          background: "linear-gradient(45deg, #3f51b5, #1a237e)",
+          boxShadow: 3,
+        }}
+      >
         <Container>
           <Toolbar disableGutters>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2, display: { xs: "flex", md: "none" } }}
+              onClick={toggleDrawer}
+            >
+              <MenuIcon />
+            </IconButton>
             <SportsHockeyIcon sx={{ mr: 1 }} />
             <Typography
               variant="h6"
@@ -58,7 +92,7 @@ function NavBar() {
             >
               Trade Friendly
             </Typography>
-            <Box sx={{ display: "flex", gap: 2 }}>
+            <Box sx={{ display: { xs: "none", md: "flex" }, gap: 2 }}>
               <Button color="inherit" component={Link} to="/">
                 Home
               </Button>
@@ -71,14 +105,7 @@ function NavBar() {
               <Button color="inherit" component={Link} to="/draftTracker">
                 Draft Tracker
               </Button>
-
-              {/* Tool Menu */}
-              <Button
-                color="inherit"
-                onClick={handleToolMenuClick}
-                aria-controls={isToolMenuOpen ? "tool-menu" : undefined}
-                aria-haspopup="true"
-              >
+              <Button color="inherit" onClick={handleToolMenuClick}>
                 Tool
               </Button>
               <Menu
@@ -92,24 +119,23 @@ function NavBar() {
                   to="/tradeSimulator"
                   onClick={handleToolMenuClose}
                 >
-                  Trade Simulator
+                  <ListItemIcon>
+                    <SettingsIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Trade Simulator" />
                 </MenuItem>
                 <MenuItem
                   component={Link}
                   to="/fantasyPool"
                   onClick={handleToolMenuClose}
                 >
-                  Fantasy Pool
+                  <ListItemIcon>
+                    <SettingsIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Fantasy Pool" />
                 </MenuItem>
               </Menu>
-
-              {/* Admin Menu */}
-              <Button
-                color="inherit"
-                onClick={handleAdminMenuClick}
-                aria-controls={isAdminMenuOpen ? "admin-menu" : undefined}
-                aria-haspopup="true"
-              >
+              <Button color="inherit" onClick={handleAdminMenuClick}>
                 Admin
               </Button>
               <Menu
@@ -123,20 +149,78 @@ function NavBar() {
                   to="/admin/rumors"
                   onClick={handleAdminMenuClose}
                 >
-                  Rumors
+                  <ListItemIcon>
+                    <AdminPanelSettingsIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Rumors" />
                 </MenuItem>
                 <MenuItem
                   component={Link}
                   to="/players"
                   onClick={handleAdminMenuClose}
                 >
-                  Players
+                  <ListItemIcon>
+                    <AdminPanelSettingsIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Players" />
                 </MenuItem>
               </Menu>
             </Box>
           </Toolbar>
         </Container>
       </AppBar>
+
+      {/* Drawer for Mobile View */}
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleDrawer}
+          onKeyDown={toggleDrawer}
+        >
+          <List>
+            <ListItem button component={Link} to="/">
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem button component={Link} to="/teams">
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Teams" />
+            </ListItem>
+            <ListItem button component={Link} to="/rumors">
+              <ListItemIcon>
+                <ListAltIcon />
+              </ListItemIcon>
+              <ListItemText primary="Rumors" />
+            </ListItem>
+            <ListItem button component={Link} to="/draftTracker">
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Draft Tracker" />
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItem button onClick={handleToolMenuClick}>
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Tool" />
+            </ListItem>
+            <ListItem button onClick={handleAdminMenuClick}>
+              <ListItemIcon>
+                <AdminPanelSettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Admin" />
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
     </>
   );
 }
